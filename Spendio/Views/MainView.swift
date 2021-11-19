467d2018e5adf0723c41fda5a558912ae1233202
khha20 <khha20qz@student.ju.se>
2021-11-19 15:36:49 +0100

    test commit for pull request

---
+++ Spendio/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Spendio
+//
+//  Created by Hasan Khalaf on 2021-11-19.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}

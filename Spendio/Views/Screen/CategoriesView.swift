e3d3eacb75443dfc9030a0352cd5ccc59710afb0
khha20 <khha20qz@student.ju.se>
2021-11-27 20:51:52 +0100

    Refactored CategoriesView

--- Spendio/Views/Screen/CategoriesView.swift
+++ Spendio/Views/Screen/CategoriesView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct CategoriesView: View {
-    @State private var showDeleteConfirmation: Bool = false
     @State private var editViewActive: Bool = false
     @State private var editViewCategoryId = 0 // Example value
     // Example data
@@ -19,40 +18,14 @@ struct CategoriesView: View {
                 // Always hidden & redirects to EditCategoryView on swipe action
                 NavigationLink("", isActive: $editViewActive) {
                     EmptyView() // Will be replaced with EditCategoryView()
-                }
-                .hidden()
-                .frame(width: 0, height: 0)
+                }.hidden().frame(width: 0, height: 0)
                 
-                // Categories
                 List {
                     ForEach (categories, id: \.self) { category in
-                        CategoryRowView(categoryName: category, categoryColor: Color.blue) // Example parameters
-                            .swipeActions(edge: .trailing) {
-                                Button(role: .destructive) {
-                                    showDeleteConfirmation = true
-                                } label: {
-                                    Image(systemName: "trash")
-                                }
-                                
-                            }
-                            .swipeActions(edge: .leading) {
-                                Button {
-                                    editViewActive.toggle()
-                                    editViewCategoryId = 5 // Example value
-                                } label: {
-                                    Image(systemName: "square.and.pencil")
-                                }
-                                .tint(.blue)
-                            }
-                            .confirmationDialog(
-                                "Are you sure you want to delete \(category)?",
-                                isPresented: $showDeleteConfirmation,
-                                titleVisibility: .visible) {
-                                    Button("Yes", role: .destructive) {
-                                        // TODO: call a ViewModel function to delete
-                                    }
-                                    Button("Cancel", role: .cancel) {}
-                                }
+                        CategoryRowView(editViewActive: $editViewActive,
+                                        editViewCategoryId: $editViewCategoryId,
+                                        categoryName: category,
+                                        categoryColor: Color.brown)
                     }
                 }
                 .listStyle(.grouped)
--- Spendio/Views/Shared/CategoryRowView.swift
+++ Spendio/Views/Shared/CategoryRowView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct CategoryRowView: View {
+    // View States
+    @State private var showDeleteConfirmation: Bool = false
+    @Binding var editViewActive: Bool
+    
+    // Example Data
+    @Binding var editViewCategoryId: Int
     let categoryName: String
     let categoryColor: Color
+    
     var body: some View {
         HStack {
             Circle()
@@ -17,11 +24,41 @@ struct CategoryRowView: View {
                 .frame(width: 15, height: 15)
             Text(categoryName)
         }
+        .swipeActions(edge: .trailing) {
+            Button(role: .destructive) {
+                showDeleteConfirmation = true
+            } label: {
+                Image(systemName: "trash")
+            }
+            
+        }
+        .swipeActions(edge: .leading) {
+            Button {
+                editViewActive.toggle()
+                editViewCategoryId = 5 // Example value
+            } label: {
+                Image(systemName: "square.and.pencil")
+            }
+            .tint(.blue)
+        }
+        .confirmationDialog(
+            "Are you sure you want to delete \(categoryName)?",
+            isPresented: $showDeleteConfirmation,
+            titleVisibility: .visible) {
+                Button("Yes", role: .destructive) {
+                    // TODO: call a ViewModel function to delete
+                }
+                Button("Cancel", role: .cancel) {}
+            }
     }
 }
 
 struct CategoryRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryRowView(categoryName: "Coffee", categoryColor: .brown)
+        CategoryRowView(editViewActive: .constant(false),
+                        editViewCategoryId: .constant(1),
+                        categoryName: "Coffee",
+                        categoryColor: Color.brown
+        )
     }
 }

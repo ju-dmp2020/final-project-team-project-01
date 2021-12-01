18330b111e4e218f24515e079f2cef2da77e5db6
Mattias Spångberg <spangberg.mattias@gmail.com>
2021-12-01 10:28:15 +0100

    Created center horizontally function

---
+++ Spendio/Extensions/ViewExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  ViewExtensions.swift
+//  Spendio
+//
+//  Created by Mattias Spångberg on 2021-12-01.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    
+    func centerHorizontally() -> some View {
+         return HStack {
+            Spacer()
+            self
+            Spacer()
+        }
+    }
+}

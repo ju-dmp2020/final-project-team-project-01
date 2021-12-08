5f1ba371dcc1e0f91d417db2bbd35d0d9eba7fb3
khha20 <khha20qz@student.ju.se>
2021-12-08 16:17:31 +0100

    Created a category model with validation functions

---
+++ Spendio/Models/CategoryModel.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryModel.swift
+//  Spendio
+//
+//  Created by Hasan Khalaf on 2021-12-07.
+//
+
+import Foundation
+import SwiftUI
+
+class CategoryModel: ObservableObject {
+    @Published var name: String = ""
+    @Published var color: Color = Color(.sRGB, red: 0.98, green: 0.9, blue: 0.2) // Default color
+    
+    let nameMinLength: Int = 2
+    let nameMaxLength: Int = 10
+    
+    func validateNameLength() -> Bool {
+        name.count >= nameMinLength &&  name.count <= nameMaxLength
+    }
+    
+    // Generic name / function in case more validation added
+    // using not "!" because the function is used inside .disabled
+    func validation() -> Bool {
+        !validateNameLength()
+    }
+}
